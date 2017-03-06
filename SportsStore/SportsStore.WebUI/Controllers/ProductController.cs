9f788190964b8d2da71091dd217b8148cb4d66e8
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-03-06 17:51:49 +0300

    Removed Unnecessary usings in all projects

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
-using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
