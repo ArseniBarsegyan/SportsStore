8afa586be9a5e74e5183391445a0a0b893b0542e
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-03-06 17:19:08 +0300

    Added NinjectControllerFactory

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    //Реализация пользовательской фабрики контроллеров, наследуясь от
+    //этой фабрики, используемой по умолчанию
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            //Создание контейнера
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            //Получение объекта контроллера из контейнера,
+            //используя его тип
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            //Конфигурирование контейнера
+        }
+    }
+}
