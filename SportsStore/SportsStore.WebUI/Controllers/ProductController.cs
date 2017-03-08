49eee6e9d791f5c553b9676989d0f4a1194e85b7
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-03-08 15:53:32 +0300

    Added limit of Products on page and TestMethod

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using System.Linq;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -8,14 +9,19 @@ namespace SportsStore.WebUI.Controllers
         //Ninject будет внедрять зависимость для хранилища товаров,
         //когда будет создавать экземпляр класса контроллеров
         private IProductRepository repository;
+        //4 товара на странице
+        public int PageSize = 4;
         public ProductController(IProductRepository productRepository)
         {
             repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
